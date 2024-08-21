d6d3a640ac47b8dd7c942221b0d597e88abbcafe
sefedev <iomosefe2016@gmail.com>
2024-08-21 10:16:49 +0100

    modify portfolio page

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -1,7 +1,66 @@
+import Contact from "@/components/Contact";
+import Image from "next/image";
 import React from "react";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <>
+      {/* HERO SECTION */}
+      <section className="text-left gap-8 text-white -mt-12 flex flex-col md:flex-row flex-center bg-gradient-to-b pt-12 pb-24 from-[rgba(255,156,0,1)] to-[rgba(255,51,0,1)] px-4 w-screen">
+        <div>
+          <h1 className="mb-4 text-5xl font-bold">
+            Check out what has
+            <br /> been done so far
+          </h1>
+          <p>See Projects i have worked on recently.</p>
+        </div>
+        <div className="relative">
+          <Image
+            src="/assets/images/animation.webp"
+            alt="laptop-man"
+            className="relative z-50 transform -scale-x-100"
+            width={400}
+            height={500}
+          />
+          <Image
+            src="/assets/images/irregular.png"
+            alt="irreglar"
+            className="absolute top-6 right-1"
+            width={350}
+            height={350}
+          />
+        </div>
+      </section>
+
+      {/* PORTFOLIO SECTION */}
+      <section className="flex flex-col w-full gap-16 p-6 my-20 bg-gray-200 md:p-16">
+        {[
+          "I-Varse",
+          "entryX Mobile App",
+          "SentientMate Mobile app & Website",
+          "Portfolio Website",
+          "Graphics & Motion Design",
+          "3D Product Modeling & Animation",
+        ].map((portfolio, id) => (
+          <div key={id} className="flex flex-col gap-4">
+            <div className="w-full h-[20rem] bg-gray-50 rounded-md"></div>
+            <div className="flex flex-col gap-4 md:items-center md:flex-row md:justify-between">
+              <span className="md:text-left">
+                <h3 className="text-2xl font-semibold">{portfolio}</h3>
+                <p>Visit to view case study</p>
+              </span>
+              <button className="px-8 py-4 text-white transition-all duration-300 rounded-md bg-gradient-to-r bg-primary-orange hover:bg-primary-orange/60">
+                Click to open
+              </button>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      {/* CONTACT SECTION */}
+      <Contact />
+    </>
+  );
 };
 
 export default Portfolio;
---
+++ components/Contact.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import React from "react";
+
+const Contact = () => {
+  return (
+    <section className="flex flex-col w-screen gap-12 px-4 py-16 bg-orange-400 md:px-32 md:flex-row md:justify-center md:items-center">
+      <div className="flex flex-col flex-1 gap-4 text-white">
+        <Image
+          src="/assets/images/marketing.png"
+          alt="Marketing"
+          width={300}
+          height={300}
+        />
+        <span className="flex flex-col gap-2 text-left text-wrap">
+          <h3 className="text-4xl font-semibold">Get Intouch</h3>
+          <p className="text-xs">
+            interested in discussing a graphic design project with us. Schedule
+            a time to chat? Looking forward to hearing from you!
+          </p>
+        </span>
+      </div>
+
+      <div className="flex flex-col flex-1 rounded-md bg-[rgba(41,45,56,1)]">
+        <form>
+          <div className="flex flex-col gap-4 p-4 py-8 md:p-8 ">
+            <input type="text" placeholder="Name" className="p-4 rounded-md" />
+            <input type="text" placeholder="Email" className="p-4 rounded-md" />
+            <textarea
+              rows="4"
+              cols="50"
+              placeholder="Message"
+              className="p-4 rounded-md"
+              id="myTextarea"
+              name="myTextarea"
+            ></textarea>
+            <div className="justify-end mt-4 md:flex">
+              <button className="w-full px-12 py-4 transition-all duration-300 bg-white border rounded-md md:w-fit hover:bg-inherit hover:text-white">
+                Submit
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
